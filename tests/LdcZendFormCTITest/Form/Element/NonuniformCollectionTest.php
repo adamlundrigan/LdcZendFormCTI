ccb0a7ee9b801f91a9b6b5889ae039d31fc7bb45
Adam Lundrigan <adam@lundrigan.ca>
2014-06-14 21:21:20 -0230

    Extract common data construction tasks into base test case

--- tests/LdcZendFormCTITest/Form/Element/NonuniformCollectionTest.php
+++ tests/LdcZendFormCTITest/Form/Element/NonuniformCollectionTest.php
@@ -2,76 +2,17 @@
 namespace LdcZendFormCTITest\Form\Element;
 
 use LdcZendFormCTITest\TestCase;
-use Zend\Stdlib\Hydrator\ClassMethods;
-use Zend\Form\Form;
-use LdcZendFormCTI\Form\Element\NonuniformCollection;
-use LdcZendFormCTITest\Form\Element\TestAssets\Entity\AccountEntity;
-use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\AccountFieldset;
-use LdcZendFormCTITest\Form\Element\TestAssets\Entity\RoleAEntity;
-use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\RoleAFieldset;
-use LdcZendFormCTITest\Form\Element\TestAssets\Entity\RoleBEntity;
-use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\RoleBFieldset;
 
 class NonuniformCollectionTest extends TestCase
 {
+
     public function testBasicSmokeTestForDataExtractionFromPrebuiltEntityStructure()
     {
         $someFakeData = $this->getTestingDataset();
 
-        // Build the test entity structure
-        $roles[0] = new RoleBEntity();
-        $roles[0]->setId($someFakeData['account']['roles'][0]['id']);
-        $roles[0]->setType($someFakeData['account']['roles'][0]['type']);
-        $roles[0]->setB($someFakeData['account']['roles'][0]['b']);
+        $obj = $this->getTestingEntity();
 
-        $roles[1] = new RoleAEntity();
-        $roles[1]->setId($someFakeData['account']['roles'][1]['id']);
-        $roles[1]->setType($someFakeData['account']['roles'][1]['type']);
-        $roles[1]->setA($someFakeData['account']['roles'][1]['a']);
-
-        $roles[2] = new RoleBEntity();
-        $roles[2]->setId($someFakeData['account']['roles'][2]['id']);
-        $roles[2]->setType($someFakeData['account']['roles'][2]['type']);
-        $roles[2]->setB($someFakeData['account']['roles'][2]['b']);
-
-        $obj = new AccountEntity();
-        $obj->setId($someFakeData['account']['id']);
-        $obj->setName($someFakeData['account']['name']);
-        $obj->setRoles($roles);
-
-        // Build the form
-
-        $form = new Form();
-
-        // Add the 'account' fieldset as the base fieldset
-        $fsAccount = new AccountFieldset();
-        $fsAccount->setUseAsBaseFieldset(true);
-        $fsAccount->setHydrator(new ClassMethods(false));
-        $fsAccount->setObject(new AccountEntity());
-
-        // Create a fieldset for each role type, and seed them with the prototype object and hydrator
-
-        $fsRoleA = new RoleAFieldset();
-        $fsRoleA->setObject(new RoleAEntity());
-        $fsRoleA->setHydrator(new ClassMethods(false));
-
-        $fsRoleB = new RoleBFieldset();
-        $fsRoleB->setObject(new RoleBEntity());
-        $fsRoleB->setHydrator(new ClassMethods(false));
-
-        // Build the NonuniformCollection by telling it what entitiy types to expect and
-        // what fieldset is associated with each one.  Attach it to the Account fieldset
-        $collAccountRoles = new NonuniformCollection();
-        $collAccountRoles->setName('roles');
-        $collAccountRoles->setDiscriminatorFieldName('type');
-        $collAccountRoles->setTargetElement(array(
-            'a' => $fsRoleA,
-            'b' => $fsRoleB,
-        ));
-        $fsAccount->add($collAccountRoles);
-
-        $form->add($fsAccount);
-
+        $form = $this->getTestingForm();
         $form->bind($obj);
         $form->isValid();
 
@@ -81,37 +22,7 @@ class NonuniformCollectionTest extends TestCase
 
     public function testBasicSmokeTestForEntityHydrationFromAlreadyPreprocessedFormData()
     {
-
-        $form = new Form();
-
-        // Add the 'account' fieldset as the base fieldset
-        $fsAccount = new AccountFieldset();
-        $fsAccount->setUseAsBaseFieldset(true);
-        $fsAccount->setHydrator(new ClassMethods(false));
-        $fsAccount->setObject(new AccountEntity());
-
-        // Create a fieldset for each role type, and seed them with the prototype object and hydrator
-
-        $fsRoleA = new RoleAFieldset();
-        $fsRoleA->setObject(new RoleAEntity());
-        $fsRoleA->setHydrator(new ClassMethods(false));
-
-        $fsRoleB = new RoleBFieldset();
-        $fsRoleB->setObject(new RoleBEntity());
-        $fsRoleB->setHydrator(new ClassMethods(false));
-
-        // Build the NonuniformCollection by telling it what entitiy types to expect and
-        // what fieldset is associated with each one.  Attach it to the Account fieldset
-        $collAccountRoles = new NonuniformCollection();
-        $collAccountRoles->setName('roles');
-        $collAccountRoles->setDiscriminatorFieldName('type');
-        $collAccountRoles->setTargetElement(array(
-            'a' => $fsRoleA,
-            'b' => $fsRoleB,
-        ));
-        $fsAccount->add($collAccountRoles);
-
-        $form->add($fsAccount);
+        $form = $this->getTestingForm();
 
         $someFakeData = $this->getTestingDataset();
 
@@ -139,33 +50,6 @@ class NonuniformCollectionTest extends TestCase
         $this->assertEquals($someFakeData['account']['roles'][2]['b'], $roles[2]->getB());
     }
 
-    public function getTestingDataset()
-    {
-        return array(
-            'account' => array(
-                'id' => 123,
-                'name' => 'Foobar Bazbat',
-                'roles' => array(
-                    array(
-                        'id' => 42,
-                        'type' => 'b',
-                        'b' => 'foo',
-                    ),
-                    array(
-                        'id' => 18,
-                        'type' => 'a',
-                        'a' => 'bar',
-                    ),
-                    array(
-                        'id' => 99,
-                        'type' => 'b',
-                        'b' => 'baz',
-                    ),
-                ),
-            ),
-        );
-    }
-
     /**
      * @dataProvider providerTestTemplateElementsArePreparedCorrectly
      *
@@ -174,39 +58,10 @@ class NonuniformCollectionTest extends TestCase
      */
     public function testTemplateElementsArePreparedCorrectly($shouldPrepareTemplate, $expectedName)
     {
-        $form = new Form();
-
-        // Add the 'account' fieldset as the base fieldset
-        $fsAccount = new AccountFieldset();
-        $fsAccount->setUseAsBaseFieldset(true);
-        $fsAccount->setHydrator(new ClassMethods(false));
-        $fsAccount->setObject(new AccountEntity());
-
-        // Create a fieldset for each role type, and seed them with the prototype object and hydrator
-
-        $fsRoleA = new RoleAFieldset();
-        $fsRoleA->setObject(new RoleAEntity());
-        $fsRoleA->setHydrator(new ClassMethods(false));
-
-        $fsRoleB = new RoleBFieldset();
-        $fsRoleB->setObject(new RoleBEntity());
-        $fsRoleB->setHydrator(new ClassMethods(false));
-
-        // Build the NonuniformCollection by telling it what entitiy types to expect and
-        // what fieldset is associated with each one.  Attach it to the Account fieldset
-        $collAccountRoles = new NonuniformCollection();
-        $collAccountRoles->setName('roles');
-        $collAccountRoles->setDiscriminatorFieldName('type');
-        $collAccountRoles->setTargetElement(array(
-            'a' => $fsRoleA,
-            'b' => $fsRoleB,
-        ));
-        $collAccountRoles->setShouldCreateTemplate($shouldPrepareTemplate);
-        $fsAccount->add($collAccountRoles);
-        $form->add($fsAccount);
+        $form = $this->getTestingForm();
         $form->prepare();
 
-        $templates = $collAccountRoles->getTemplateElement();
+        $templates = $form->get('account')->get('roles')->getTemplateElement();
         foreach ($templates as $objTemplate) {
             $this->assertEquals($expectedName, $objTemplate->getName());
         }
@@ -219,4 +74,5 @@ class NonuniformCollectionTest extends TestCase
             array( false, '__index__' ),
         );
     }
+
 }
--- tests/LdcZendFormCTITest/TestCase.php
+++ tests/LdcZendFormCTITest/TestCase.php
@@ -3,6 +3,16 @@ namespace LdcZendFormCTITest;
 
 use Zend\ServiceManager\ServiceManager;
 use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\Stdlib\Hydrator\ClassMethods;
+use Zend\Form\Form;
+use LdcZendFormCTI\InputFilter\NonuniformCollectionInputFilter;
+use LdcZendFormCTI\Form\Element\NonuniformCollection;
+use LdcZendFormCTITest\Form\Element\TestAssets\Entity\AccountEntity;
+use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\AccountFieldset;
+use LdcZendFormCTITest\Form\Element\TestAssets\Entity\RoleAEntity;
+use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\RoleAFieldset;
+use LdcZendFormCTITest\Form\Element\TestAssets\Entity\RoleBEntity;
+use LdcZendFormCTITest\Form\Element\TestAssets\Fieldset\RoleBFieldset;
 
 /**
  * Base test case to be used when a service manager instance is required
@@ -56,4 +66,108 @@ class TestCase extends \PHPUnit_Framework_TestCase
 
         return $serviceManager;
     }
+
+    public function getTestingDataset()
+    {
+        return array(
+            'account' => array(
+                'id' => 123,
+                'name' => 'Foobar Bazbat',
+                'roles' => array(
+                    array(
+                        'id' => 42,
+                        'type' => 'b',
+                        'b' => 'foo',
+                    ),
+                    array(
+                        'id' => 18,
+                        'type' => 'a',
+                        'a' => 'bar',
+                    ),
+                    array(
+                        'id' => 99,
+                        'type' => 'b',
+                        'b' => 'baz',
+                    ),
+                ),
+            ),
+        );
+    }
+
+    public function getTestingEntity()
+    {
+        $someFakeData = $this->getTestingDataset();
+
+        // Build the test entity structure
+        $roles[0] = new RoleBEntity();
+        $roles[0]->setId($someFakeData['account']['roles'][0]['id']);
+        $roles[0]->setType($someFakeData['account']['roles'][0]['type']);
+        $roles[0]->setB($someFakeData['account']['roles'][0]['b']);
+
+        $roles[1] = new RoleAEntity();
+        $roles[1]->setId($someFakeData['account']['roles'][1]['id']);
+        $roles[1]->setType($someFakeData['account']['roles'][1]['type']);
+        $roles[1]->setA($someFakeData['account']['roles'][1]['a']);
+
+        $roles[2] = new RoleBEntity();
+        $roles[2]->setId($someFakeData['account']['roles'][2]['id']);
+        $roles[2]->setType($someFakeData['account']['roles'][2]['type']);
+        $roles[2]->setB($someFakeData['account']['roles'][2]['b']);
+
+        $obj = new AccountEntity();
+        $obj->setId($someFakeData['account']['id']);
+        $obj->setName($someFakeData['account']['name']);
+        $obj->setRoles($roles);
+
+        return $obj;
+    }
+
+    public function getTestingForm()
+    {
+        $form = new Form();
+
+        // Add the 'account' fieldset as the base fieldset
+        $fsAccount = new AccountFieldset();
+        $fsAccount->setUseAsBaseFieldset(true);
+        $fsAccount->setHydrator(new ClassMethods(false));
+        $fsAccount->setObject(new AccountEntity());
+
+        // Create a fieldset for each role type, and seed them with the prototype object and hydrator
+
+        $fsRoleA = new RoleAFieldset();
+        $fsRoleA->setObject(new RoleAEntity());
+        $fsRoleA->setHydrator(new ClassMethods(false));
+
+        $fsRoleB = new RoleBFieldset();
+        $fsRoleB->setObject(new RoleBEntity());
+        $fsRoleB->setHydrator(new ClassMethods(false));
+
+        // Build the NonuniformCollection by telling it what entitiy types to expect and
+        // what fieldset is associated with each one.  Attach it to the Account fieldset
+        $collAccountRoles = new NonuniformCollection();
+        $collAccountRoles->setName('roles');
+        $collAccountRoles->setDiscriminatorFieldName('type');
+        $collAccountRoles->setTargetElement(array(
+            'a' => $fsRoleA,
+            'b' => $fsRoleB,
+        ));
+        $fsAccount->add($collAccountRoles);
+
+        $form->add($fsAccount);
+
+        return $form;
+    }
+
+    public function getTestingInputFilter()
+    {
+        $inputFilter = new NonuniformCollectionInputFilter();
+        $inputFilter->setDiscriminatorFieldName('type');
+        $inputFilter->setInputFilter(array(
+            'a' => new InputFilter\TestAssets\RoleAInputFilter(),
+            'b' => new InputFilter\TestAssets\RoleBInputFilter(),
+        ));
+
+        return $inputFilter;
+    }
+
 }
