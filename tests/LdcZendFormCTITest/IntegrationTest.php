cdc7bc65d09f7e66e313579084616cd001e56102
Adam Lundrigan <adam@lundrigan.ca>
2014-06-14 21:32:25 -0230

    Basic integration test to tie it all together

---
+++ tests/LdcZendFormCTITest/IntegrationTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace LdcZendFormCTITest;
+
+class IntegrationTest extends TestCase
+{
+    public function testEntityToArrayHappyCase()
+    {
+        $entity = $this->getTestingEntity();
+        
+        $baseFilter = new \Zend\InputFilter\InputFilter;
+        $accountFilter = new \Zend\InputFilter\InputFilter;
+        $accountFilter->add($this->getTestingInputFilter(), 'roles');
+        $baseFilter->add($accountFilter, 'account');
+        
+        $form = $this->getTestingForm();
+        $form->setInputFilter($baseFilter);
+        $form->bind($entity);
+        $form->isValid();
+        
+        $extractedData = $form->getData(\Zend\Form\FormInterface::VALUES_AS_ARRAY);
+        $this->assertEquals($this->getTestingDataset(), $extractedData);        
+    }
+    
+    public function testArrayToEntityHappyCase()
+    {
+        $baseFilter = new \Zend\InputFilter\InputFilter;
+        $accountFilter = new \Zend\InputFilter\InputFilter;
+        $accountFilter->add($this->getTestingInputFilter(), 'roles');
+        $baseFilter->add($accountFilter, 'account');
+        
+        $form = $this->getTestingForm();
+        $form->setInputFilter($baseFilter);
+        $form->setData($this->getTestingDataset());
+        $form->isValid();
+        
+        $extractedEntity = $form->getData();
+        $this->assertEquals($this->getTestingEntity(), $extractedEntity);        
+    }
+}
